a2745830cfa04a78d2739c994e33c31ade1792d0
fernandoleiva94 <TuNuevoEmail@example.com>
2026-01-02 16:12:06 -0300

    add Jackson configuration to prevent lazy loading during serialization; update pom.xml to include jackson-datatype-hibernate6 dependency; modify Company class to ignore Hibernate properties

--- src/main/java/com/sevenb/invoices/model/Company.java
+++ src/main/java/com/sevenb/invoices/model/Company.java
@@ -1,6 +1,7 @@
 package com.sevenb.invoices.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,6 +14,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Company {
 
     @Id
--- target/generated-sources/annotations/com/sevenb/invoices/mapper/InvoiceMapperImpl.java
+++ target/generated-sources/annotations/com/sevenb/invoices/mapper/InvoiceMapperImpl.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-01-02T14:44:36-0300",
+    date = "2026-01-02T16:10:23-0300",
     comments = "version: 1.6.2, compiler: javac, environment: Java 17.0.15 (Ubuntu)"
 )
 public class InvoiceMapperImpl implements InvoiceMapper {
